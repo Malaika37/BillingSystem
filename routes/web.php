2ef159b6151d69481674a9165212b76d7cda110b
Malaika37 <minhajashfaq40@gmail.com>
2026-07-30 07:37:52 +0500

    cutomer record

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BillController;
+use App\Http\Controllers\CustomerController;
 
 // Route::get('/', function () {
 //     return view('invoice');
@@ -12,3 +13,6 @@ Route::post('/bills',[BillController::class, 'store'])->name('bills.store');
 Route::get('/bills/{bill}', [BillController::class, 'show'])->name('bills.show');
 Route::get('/bills/{bill}/pdf',[BillController::class, 'generatePdf'])->name('bills.pdf');
 Route::post('/bills/{bill}/whatsapp', [BillController::class, 'sendWhatsApp'])->name('bills.whatsapp');
+Route::get('/customers',[CustomerController::class, 'index'])->name('customers.index');
+Route::get('/customers/{customer}',[CustomerController::class, 'show'])->name('customers.show');
+Route::get('/bills/{bill}',[CustomerController::class, 'showBill'])->name('showbill.show');
